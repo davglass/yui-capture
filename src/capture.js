17a18f1314d3d558e201789ea77a80c4dd8ef172
Dav Glass <davglass@gmail.com>
2008-12-31 13:18:45 -0800

    Setup

---
+++ src/capture.js
@@ -0,0 +1,14 @@
+(function() {
+    var Dom = YAHOO.util.Dom,
+        Event = YAHOO.util.Event;
+
+    var Capture = function() {
+    };
+
+    var proto = {
+    };
+
+    Capture.prototype = proto;
+    YAHOO.util.Capture = Capture;
+
+})();
